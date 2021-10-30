cf8055e0c445850e00c60cbed741be23069dee92
rizwan3711 <84203059+rizwan3711@users.noreply.github.com>
2021-10-30 00:32:43 -0700

    Initial Commit

---
+++ Gradebook/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Gradebook
+//
+//  Created by Rizwan Syed on 10/30/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
