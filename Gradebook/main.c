a87bf3fe1f4abc9e0d145f354ee121d3307667bd
rizwan3711 <84203059+rizwan3711@users.noreply.github.com>
2021-11-01 09:30:57 -0700

    second commit

--- Gradebook/main.c
+++ Gradebook/main.c
@@ -7,8 +7,22 @@
 
 #include <stdio.h>
 
+char** inputCategories(){
+    const char *cats[10];
+    int i = 0;
+    char input[50];
+    printf("Enter the categories of your class, pressing enter each category. Press ` when you are done.\n");
+    while(input != "`"){
+        scanf("%s", input);
+        cats[i] = input;
+        i++;
+    }
+    return *cats;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Commit Testing\n");
+    char *catgories[10];
+    categories = inputCategories();
     return 0;
 }
