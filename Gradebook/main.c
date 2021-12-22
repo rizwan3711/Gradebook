34bda128f63af6e70d7458a0114ec363d1e4e028
rizwan3711 <84203059+rizwan3711@users.noreply.github.com>
2021-12-21 21:07:43 -0800

    fixed bug where when classes were printed spaces were new line characters

--- Gradebook/main.c
+++ Gradebook/main.c
@@ -11,21 +11,84 @@
 // TODO: ask user for list of classes and store it, so you are making an array of character arrays
 // TODO: state the number of classes, and ask them to enter the corresponding number in order to enter that class
 // TODO: when in a class menu, have a button that allows the user to go back, and another button which calculates the weight of
+typedef struct{
+    char name[50];
+    int numWords;
+} class;
+
 void classWriter(FILE *pointer){
+    
     pointer = fopen("gradebook.txt", "w");
-    printf("Please enter the names of your classes, pressing enter after each class. When you are finished enterring your classes, press the ` key, and then press enter.\n");
+    printf("Please enter the names of your classes, pressing enter after each class(< 50 characters). When you are finished enterring your classes, press the ` key, and then press enter.\n");
     char input[50];
-    scanf("%s", &input);
-    printf("\n");
-    while(strcmp(input, "`") != 0){
-        //write to the text file
-        fprintf(pointer, "%s\n", input);
-        scanf("%s", &input);
-        printf("\n");
+    //scanf("%[^\n]s", input);
+    fgets(input, sizeof(input), stdin);
+    
+    /*
+    int i = 0;
+    while(input[i] != '\0'){
+        if(input[i] == ' '){
+            input[i] = '_';
+        }
+        i++;
     }
+    */
+    /*
+    printf("\n");
+    printf("%s", input);
+    printf("\n");
+    printf("%d", strcmp(input, "`"));
+    printf("\n");
+     */
+    int i = 0;
+    while(input[i] != '\0'){
+        if(input[i] == '\n'){
+            input[i] = '\0';
+        }
+        i++;
+    }
+    
+    while(strcmp(input, "`") != 0){
+        
+        fprintf(pointer, "%s\n", input);
+        
+        
+        //scanf("%[^\n]s", input);
+        fgets(input, sizeof(input), stdin);
+        
+        /*
+        printf("\n");
+        printf("%s", input);
+        printf("\n");
+        printf("%d", strcmp(input, "`"));
+        printf("\n");
+         */
+        
+        /*
+        int i = 0;
+        while(input[i] != '\0'){
+            if(input[i] == ' '){
+                input[i] = '_';
+                printf("space found\n");
+            }
+            i++;
+        }
+         */
+        int i = 0;
+        while(input[i] != '\0'){
+            if(input[i] == '\n'){
+                input[i] = '\0';
+            }
+            i++;
+        }
+        
+    }
+    
 }
 
+
 int main(){
+    //remove("gradebook.txt");
     FILE *fptr;
     fptr = fopen("gradebook.txt", "r");
     if(fptr == NULL){
@@ -33,16 +96,33 @@ int main(){
         //function which writes classes to files
         classWriter(fptr);
     }
+    
     else{
         //give the user the option to either view one of their classes, or edit the list of classes which they currently have
-        printf("It seems you have used this program before! Here are the classes you currently have enterred in the gradebook: \n");
+        printf("It seems you have used this program before! Here are the classes you currently have enterred in the gradebook: \n\n");
         fptr = fopen("gradebook.txt", "r");
         char line[256];
+        
         while( fgets(line, sizeof(line), fptr) ){
-            printf("%s\n" , line);
+            //int i = 0;
+            /*
+            while(line[i] != '\0'){
+                if(line[i] == '_'){
+                    line[i] = ' ';
+                }
+                i++;
+            }
+             */
+            printf("%s" , line);
         }
+        printf("\n");
+        
     }
+    
     fclose(fptr);
-   
-        return 0;
+
+    //remove("gradebook.txt");
+    return 0;
+    
 }
+
